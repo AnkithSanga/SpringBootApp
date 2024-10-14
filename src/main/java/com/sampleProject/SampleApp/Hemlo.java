bb6f0ff031c1c2b7f6d33830d96b11864605c50e
Ankith Sanga <ankithsanga1234@gmail.com>
2024-10-15 00:33:21 +0530

    Temp commit

--- src/main/java/com/sampleProject/SampleApp/Hemlo.java
+++ src/main/java/com/sampleProject/SampleApp/Hemlo.java
@@ -2,5 +2,7 @@ package com.sampleProject.SampleApp;
 
 public class Hemlo {
 
-    public static void main
+    public static void main (String[] args){
+        System.out.println("This is a print message");
+    }
 }
